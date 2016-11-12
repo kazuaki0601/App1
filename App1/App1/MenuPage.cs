18bfb3d87de6053b82b01fed5cc9c41658cdb58e
tachikawa <tachikawa@DESKTOP-AE0O2DA>
2016-11-12 10:42:40 +0900

    プロジェクト ファイルを追加します。

---
+++ App1/App1/MenuPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            var label = new Label();
+            var btnNext = new Button()
+            {
+                Text = "次へ"
+            };
+            btnNext.Clicked += btnNext_Clicked;
+            label.HorizontalTextAlignment = TextAlignment.Center;
+
+            // The root page of your application
+
+            var stackLayout = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        },btnNext,
+                        label
+                    }
+            };
+            
+            this.Content = stackLayout;
+        }
+
+
+        void btnNext_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
